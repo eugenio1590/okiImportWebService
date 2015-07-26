5c46fab26394b11ae632109b7c5c6e65c8d880f1
Usuario <eugeniohernandez17@gmail.com>
2015-07-25 23:43:13 -0430

    Encriptacion de la clave de la conexion con postgres y lectura de sus propiedades por medio de un archivo.

--- okiImportWebService/src/main/java/com/okiimport/web_service/HomeController.java
+++ okiImportWebService/src/main/java/com/okiimport/web_service/HomeController.java
@@ -1,9 +1,16 @@
 package com.okiimport.web_service;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Properties;
 
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.EnvironmentStringPBEConfig;
+import org.jasypt.properties.EncryptableProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -33,7 +40,53 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
+		//cargarPropiedades();
+		
 		return "home";
+	}
+	
+	private String getPath(){
+		return this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath().split("WEB-INF")[0];
+	}
+	
+	private void cargarPropiedades(){
+		EnvironmentStringPBEConfig environmentVariablesConfiguration = new EnvironmentStringPBEConfig();
+		environmentVariablesConfiguration.setAlgorithm("PBEWithMD5AndDES");
+		environmentVariablesConfiguration.setPasswordEnvName("APP_ENCRYPTION_PASSWORD");
+		/*
+		  * First, create (or ask some other component for) the adequate encryptor for
+		  * decrypting the values in our .properties file.
+		  */
+		 StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+		 //encryptor.setPassword("euge17"); // could be got from web, env variable...
+		 encryptor.setConfig(environmentVariablesConfiguration);
+		 
+		 /*
+		  * Create our EncryptableProperties object and load it the usual way.
+		  */
+		 Properties props = new EncryptableProperties(encryptor);
+		 try {
+			props.load(new FileInputStream(getPath()+"WEB-INF/classes/jdbc.properties"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		 /*
+		  * To get a non-encrypted value, we just get it with getProperty...
+		  */
+		 String datasourceUsername = props.getProperty("datasource.username");
+		 System.out.println(datasourceUsername);
+
+		 /*
+		  * ...and to get an encrypted value, we do exactly the same. Decryption will
+		  * be transparently performed behind the scenes.
+		  */ 
+		 String datasourcePassword = props.getProperty("datasource.password");
+		 System.out.println(datasourcePassword);
 	}
 	
 }
